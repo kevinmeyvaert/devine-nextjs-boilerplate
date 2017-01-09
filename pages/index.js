2b16e3827c52cf9d7bef2e441ba67fff80e2aead
kevinmeyvaert <kevin.meyvaert@gmail.com>
2017-01-09 16:51:54 +0100

    :art: Made Home stateless function

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
 
-export default class extends React.Component {
-  // meer info over lifecycle en server/client behavior
-  // te vinden op https://github.com/zeit/next.js#fetching-data-and-component-lifecycle
-  static async getInitialProps ({req}) {
-    return req
-      ? {userAgent: req.headers[`user-agent`]}
-      : {userAgent: navigator.userAgent};
-  }
-  render () {
-    return (
+const Home = () => {
+  return (
     <div>
       <Navigation />
       Hallo nerdy Deviner!
     </div>
-    );
-  }
-}
+  );
+};
+
+export default Home;
